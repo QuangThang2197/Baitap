2e2918c0fc60e469de678e7df2166d96f5a10476
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2020-12-19 23:13:55 +0700

    Update bt-bar.py

--- bt-bar.py
+++ bt-bar.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-color_names = ['Ngan', 'Quynh', 'Hung', '', 'Long']
+color_names = ['Ngân', 'Quỳnh', 'Hưng', 'Thi', 'Long']
 colors = ['#3498db', '#fd79a8', '#f1c40f', '#273c75', '#e74c3c']
 num_favorite = [7,5,10,9,6]
 xs = [i + 0.03 for i, _ in enumerate(color_names)]
 plt.bar(xs, num_favorite, color=colors)
-plt.title('Hoc sinh lop 9')
+plt.title('Học sinh lớp 9')
 plt.xticks([i+0.1 for i, _ in enumerate(color_names)], color_names)
-plt.xlabel('Ten')
-plt.ylabel('Diem')
+plt.xlabel('Tên học sinh')
+plt.ylabel('Điểm')
 plt.show()
