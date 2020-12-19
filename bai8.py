fc7a49bc5c8acbd2118c7dfef4b51e12f29b18b5
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2020-12-19 23:19:24 +0700

    Update bai8.py

--- bai8.py
+++ bai8.py
@@ -1,14 +1,13 @@
 import smtplib
-
-my_email = input("nhap email cua ban: ")
-receive_email = input("nhap email nhan: ")
-password = input(str("pass: "))
-message = input("nhap noi dung: ")
-n = int(input("so lan ban muon gui: "))
+my_email = input('Nhập email của bạn: ')
+receive_email = input('Nhập email nhận: ')
+password = input(str('Mật khẩu: '))
+message = input('Nội dung: ')
+n = int(input('Số lần bạn muốn gửi: '))
 for i in range(n):
  sever = smtplib.SMTP('smtp.gmail.com',587)
  sever.starttls()
  sever.login(my_email,password)
- print("thanh cong")
+ print('Thành công')
  sever.sendmail(my_email,receive_email,message)
- print("da gui den:",receive_email)
+ print('Đã gửi đến:',receive_email)
