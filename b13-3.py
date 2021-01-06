563dc0689fd32aa5a25157cbdf2b695e93b80423
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-01-06 15:47:20 +0700

    Update b13-3.py

--- b13-3.py
+++ b13-3.py
@@ -1,30 +1,17 @@
-import os, random, string, math
+import os,string,random,math
 
-def get_size(start_path = '.'):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
-
-def random_string(length):
-    letters = string.ascii_lowercase + string.ascii_uppercase + '0123456789'
-    return ''.join(random.choice(letters) for i in range(length))
-
-
-foldername = str(input('Nhập tên thư mục: '))
-filename = str(input('Nhập tên tệp tin: '))
-don_vi = 1048576  #1kb tinh theo byte
-size = don_vi * float(input('Nhập dung lượng dữ liệu giới hạn là 1MB <= size <= 1024MB: '))
-os.mkdir('C:\%s' %foldername)
-os.chdir('C:\%s' %foldername)
-for i in range(int(size/1048576)+1):
-    if float(get_size('C:\%s' %foldername)) <= size-1024*1000:
-        f = open(filename+'%d' %i,'a')
-        f.write(random_string(1024*1000))
-        f.close()
-    else:
-        f = open(filename+'%d' %i,'a')
-        f.write(random_string(int(size)-get_size('C:\%s' %foldername)))
-        f.close()
+num = 1000
+limit = int(input("NHAP GIOI HAN DU LIEU 1-1024MB: "))
+if limit >= 1 and limit <= 1024:
+ for i in range(int(limit * 1024 / capa)):
+    file = open('file' + str(i + 1) + '.txt', 'w')
+    file.write(random.choice(string.ascii_letters))
+ if int(limit* 1024 % num) >0 :
+    file = open('file end ' + '.txt', 'w')
+    file.write(random.choice(string.ascii_letters))
+ if file.seek(0):
+    os.remove(file)
+ file.close()
+ print("thanh cong")
+else:
+    print("Sai gioi han")
