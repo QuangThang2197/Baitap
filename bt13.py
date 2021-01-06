a6659bc8e8d1b3c3ce1390288559f22830f7d83e
thang1230 <72205096+Hoang-Quang-Thang@users.noreply.github.com>
2021-01-06 15:27:21 +0700

    Update bt13.py

--- bt13.py
+++ bt13.py
@@ -1,14 +1,14 @@
-import os
-list1= []
-list2= []
-for (root,dirs,files) in os.walk('C:'):  # os.walk de truy cap o dia minh chon
-    print("thu muc o dia C:",root)  # root la thu muc can chon
-    print("thu muc con",dirs)  # dirs la cac thu muc con
-    print("tap tin ",files)  # files la cac tap tin
-for (root,dirs,files) in os.walk('C:'):
-    for i in files:
-        list1.append(i)
-    for x in dirs:
-        list2.append(x)
-print("list tap tin ",list1)
-print("list thu muc con ",list2)
+import os.path
+
+path = 'C:\\Users\\DELL'
+x = os.listdir(path)
+print("cac thu muc trong o dia C: ")
+print(x)
+
+list1 = next(os.walk(path))[1]
+print("cac thu muc: ")
+print(list1)
+
+list2 = next(os.walk(path))[2]
+print("cac tap tin: ")
+print(list2)
